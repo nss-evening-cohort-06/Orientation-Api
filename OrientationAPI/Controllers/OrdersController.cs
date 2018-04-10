54b207317a7b5472d301c5cc6f4e3cd6fdc6391a
Jess <jessibcoden@gmail.com>
2018-04-10 17:03:45 -0500

    created OrderDto model and Create method in OrderRepository

---
+++ OrientationAPI/Controllers/OrdersController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using OrientationAPI.Models;
+
+namespace OrientationAPI.Controllers
+{
+    [RoutePrefix("api/orders")]
+    public class OrdersController :
+        ApiController
+    {
+        [Route, HttpPost]
+        public HttpResponseMessage CreateOrder(OrderDto createOrder)
+        {
+
+        }
+
+    }
+}
