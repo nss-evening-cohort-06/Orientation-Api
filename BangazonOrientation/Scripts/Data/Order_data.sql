bd639a63ef1fa33029bd9614b960742274fd983d
ken lewis <kenneth.g.lewis2@gmail.com>
2018-04-09 21:18:20 -0500

    Added seed data for Order table, and added an attribute (Purchased) to the Order table.

---
+++ BangazonOrientation/Scripts/Data/Order_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO [dbo].[Order] ([PaymentID],[CustomerID],[Purchased]) VALUES (1,6,0);
+INSERT INTO [dbo].[Order] ([PaymentID],[CustomerID],[Purchased]) VALUES (2,5,0);
+INSERT INTO [dbo].[Order] ([PaymentID],[CustomerID],[Purchased]) VALUES (3,4,0);
+INSERT INTO [dbo].[Order] ([PaymentID],[CustomerID],[Purchased]) VALUES (4,3,0);
+INSERT INTO [dbo].[Order] ([PaymentID],[CustomerID],[Purchased]) VALUES (5,2,0);
+INSERT INTO [dbo].[Order] ([PaymentID],[CustomerID],[Purchased]) VALUES (6,1,0);
