a7dbde57ea62c778a8cfff07526d1f24e48e95d9
Gina <ginamantonini@gmail.com>
2018-04-11 18:07:26 -0500

    updated products database file

--- BangazonOrientation/Scripts/Data/Product_data.sql
+++ BangazonOrientation/Scripts/Data/Product_data.sql
@@ -1,10 +1,45 @@
-INSERT INTO [dbo].[Product] ([Name],[Price],[Owner],[Count],[Description]) VALUES (N'Vienna Sausage',14,1,1,'Juciy good pressed meat')
-INSERT INTO [dbo].[Product] ([Name],[Price],[Owner],[Count],[Description]) VALUES (N'Princess Diana Beanie Baby',200000,1,2,'Why, just why')
-INSERT INTO [dbo].[Product] ([Name],[Price],[Owner],[Count],[Description]) VALUES (N'A Somewhat Used Tire',53,2,3,'Not all the way worn down')
-INSERT INTO [dbo].[Product] ([Name],[Price],[Owner],[Count],[Description]) VALUES (N'Sound Machine',72,2,4,'Farts, burps, and the likes')
-INSERT INTO [dbo].[Product] ([Name],[Price],[Owner],[Count],[Description]) VALUES (N'Alexs Soul',14000000,3,5,'Cant hardly put a price on this')
-INSERT INTO [dbo].[Product] ([Name],[Price],[Owner],[Count],[Description]) VALUES (N'Frozen Super Size Fries',4,3,6,'Perfect for reheating already in the box')
-INSERT INTO [dbo].[Product] ([Name],[Price],[Owner],[Count],[Description]) VALUES (N'Lava Lamp',12,4,7,'A throwback to the good ole days')
-INSERT INTO [dbo].[Product] ([Name],[Price],[Owner],[Count],[Description]) VALUES (N'Inflatable Unicorn Hat',6,4,8,'')
-INSERT INTO [dbo].[Product] ([Name],[Price],[Owner],[Count],[Description]) VALUES (N'Everlasting Dry Erase Markers',204,5,9,'Really they last forever')
-INSERT INTO [dbo].[Product] ([Name],[Price],[Owner],[Count],[Description]) VALUES (N'Adams MacBook',100,5,10,'It is probably worth more, but maybe not')
+USE [SNQHM_bangazoncli_db]
+GO
+
+ALTER TABLE [dbo].[Product] DROP CONSTRAINT [FK_Owner]
+GO
+
+ALTER TABLE [dbo].[Product] DROP CONSTRAINT [DF_Product_OutOfStock]
+GO
+
+/****** Object:  Table [dbo].[Product]    Script Date: 4/11/2018 6:06:47 PM ******/
+DROP TABLE [dbo].[Product]
+GO
+
+/****** Object:  Table [dbo].[Product]    Script Date: 4/11/2018 6:06:47 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Product](
+	[ProductID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](30) NOT NULL,
+	[Price] [money] NOT NULL,
+	[Owner] [int] NOT NULL,
+	[Count] [int] NOT NULL,
+	[Description] [nvarchar](200) NULL,
+	[OutOfStock] [bit] NOT NULL,
+ CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED 
+(
+	[ProductID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Product] ADD  CONSTRAINT [DF_Product_OutOfStock]  DEFAULT ((0)) FOR [OutOfStock]
+GO
+
+ALTER TABLE [dbo].[Product]  WITH CHECK ADD  CONSTRAINT [FK_Owner] FOREIGN KEY([Owner])
+REFERENCES [dbo].[Customer] ([CustomerID])
+GO
+
+ALTER TABLE [dbo].[Product] CHECK CONSTRAINT [FK_Owner]
+GO
+
