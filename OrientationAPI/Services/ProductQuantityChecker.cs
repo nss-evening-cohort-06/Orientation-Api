7982f45ae2e7d07b02ac2287fe58df80d47cf225
Jess <jessibcoden@gmail.com>
2018-04-12 16:27:16 -0500

    added ProductQuantityChecker Service and method to check that quantity is > 0

---
+++ OrientationAPI/Services/ProductQuantityChecker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrientationAPI.Services
+{
+    public class ProductQuantityChecker
+    {
+        public static bool ProductIsAvailable(int productId)
+        {
+            var repo = new ProductRepository();
+            var product = repo.SelectProduct(productId);
+            if (product.Quantity <= 0)
+                return true;
+            return false;
+        }
+    }
+}
