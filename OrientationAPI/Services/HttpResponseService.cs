dd764f6ad5240e43f760d3cc8eb729a2959d5166
gib <gibbowden@gmail.com>
2018-04-12 14:26:24 -0500

    add payment date with validation

--- OrientationAPI/Services/HttpResponseService.cs
+++ OrientationAPI/Services/HttpResponseService.cs
@@ -18,10 +18,12 @@ namespace OrientationAPI.Services
                     return message.CreateResponse(HttpStatusCode.Created);
                 case DbResponseEnum.NotCreated:
                     return message.CreateErrorResponse(HttpStatusCode.InternalServerError, "The requested record could not be created, try again later");
-                case DbResponseEnum.NoRecordsFound:
+                case DbResponseEnum.NotFound:
                     return message.CreateErrorResponse(HttpStatusCode.NotFound, "The record could not be found");
                 case DbResponseEnum.Updated:
                     return message.CreateResponse(HttpStatusCode.OK);
+                case DbResponseEnum.ValidationError:
+                    return message.CreateErrorResponse(HttpStatusCode.BadRequest, "Validation error has occured, please check your data");
                 default:
                     return message.CreateErrorResponse(HttpStatusCode.InternalServerError, "Not sure how we got here");
             }
@@ -34,7 +36,7 @@ namespace OrientationAPI.Services
             {
                 case DbResponseEnum.RecordsReturned:
                     return message.CreateResponse(HttpStatusCode.OK, customers);
-                case DbResponseEnum.NoRecordsFound:
+                case DbResponseEnum.NotFound:
                     return message.CreateErrorResponse(HttpStatusCode.NotFound, "No records found");
                 default:
                     return message.CreateErrorResponse(HttpStatusCode.InternalServerError, "Not sure how we got here");
@@ -44,17 +46,21 @@ namespace OrientationAPI.Services
 
         public static HttpResponseMessage CreateAddRecordResponse(this HttpRequestMessage message, int dbResult)
         {
-            return dbResult == 1 ? message.MapHttpResponse(DbResponseEnum.NoRecordsFound) : message.MapHttpResponse(DbResponseEnum.NotCreated);
+            return dbResult == 1 ? message.MapHttpResponse(DbResponseEnum.Created) : message.MapHttpResponse(DbResponseEnum.NotCreated);
         }
-
+        
         public static HttpResponseMessage CreateUpdateRecordResponse(this HttpRequestMessage message, int dbResult)
         {
-            return dbResult == 1 ? message.MapHttpResponse(DbResponseEnum.Created) : message.MapHttpResponse(DbResponseEnum.NotCreated);
+            if (dbResult > -1)
+            {
+                return dbResult == 1 ? message.MapHttpResponse(DbResponseEnum.Updated) : message.MapHttpResponse(DbResponseEnum.NotFound);
+            }
+            return message.MapHttpResponse(DbResponseEnum.ValidationError);             
         }
 
         public static HttpResponseMessage CreateListRecordsResponse(this HttpRequestMessage message, IEnumerable<Customer> dbResult)
         {
-            return dbResult.Count() >= 1 ? message.MapHttpResponse(DbResponseEnum.RecordsReturned, dbResult) : message.MapHttpResponse(DbResponseEnum.NoRecordsFound);
+            return dbResult.Count() >= 1 ? message.MapHttpResponse(DbResponseEnum.RecordsReturned, dbResult) : message.MapHttpResponse(DbResponseEnum.NotFound);
         }
 
     }
