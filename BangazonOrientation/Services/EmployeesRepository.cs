3c0700872d8fa99b6afc040339629de60bc03c79
developer <matt.hensley777@gmail.com>
2018-04-23 20:37:59 -0500

    start

---
+++ BangazonOrientation/Services/EmployeesRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using Dapper;
+using BangazonOrientation.Models;
+
+namespace BangazonOrientation.Services
+{
+    public class EmployeesRepository
+    {
+        private static SqlConnection GetConnection()
+        {
+            return new SqlConnection(ConfigurationManager.ConnectionStrings["BangazonOrientation"].ConnectionString);
+        }
+
+
+        public IEnumerable<EmployeesDto> ListAllEmployees()
+        {
+            using (var db = GetConnection())
+            {
+                db.Open();
+                var getEmployeeList = db.Query<EmployeesDto>(@"SELECT  FirstName, LastName, FROM Employee");
+
+                return getEmployeeList;
+            }
+        }
+
+
+    }
+}
