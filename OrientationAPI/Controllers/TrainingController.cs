a5c207ebdaa68646603612261494d4b446ca0598
gib <gibbowden@gmail.com>
2018-04-24 15:33:13 -0500

    shelve

---
+++ OrientationAPI/Controllers/TrainingController.cs
@@ -0,0 +1,32 @@
+﻿using OrientationAPI.Models;
+using OrientationAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OrientationAPI.Controllers
+{
+    [RoutePrefix("api/training")]
+    public class TrainingController : ApiController
+    {
+        [Route, HttpPost]
+        public HttpResponseMessage Create(TrainingDto dto)
+        {
+            var repo = new TrainingRepository();
+            var dbResults = repo.Create(dto);
+            return Request.CreateAddRecordResponse(dbResults);
+        }
+
+        [Route, HttpGet]
+        public HttpResponseMessage GetAll()
+        {
+            var repo = new TrainingRepository();
+            var dbResults = repo.GetAll();
+            return Request.CreateListRecordsResponse(dbResults);
+        }
+    }
+
+}
---
+++ OrientationAPI/Services/TrainingRepository.cs
@@ -0,0 +1,50 @@
+﻿using Dapper;
+using OrientationAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace OrientationAPI.Services
+{
+    public class TrainingRepository
+    {
+        private SqlConnection getDb()
+        {
+            return new SqlConnection(ConfigurationManager.ConnectionStrings["BRBangazon"].ConnectionString);
+        }
+        public int Create(TrainingDto dto)
+        {
+            using (var db = getDb())
+            {
+                db.Open();
+                var sql = @"INSERT INTO dbo.TrainingProgram
+                            (name, description, startDate, endDate, maxAttendees)
+                            VALUES 
+                            (@name, @description, @startDate, @endDate, @maxAttendees)";
+                return db.Execute(sql, dto);
+            }
+        }
+
+        internal object GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TrainingList> GetAllActive()
+        {
+            using (var db = new SqlConnection(ConfigurationManager.ConnectionStrings["BRBangazon"].ConnectionString))
+            {
+                db.Open();
+                var sql = "Select * From dbo.TrainingProgram";
+                return db.Query<TrainingList>(sql).ToList();
+            }
+        }
+    }
+
+    public class TrainingList
+    {
+    }
+}
