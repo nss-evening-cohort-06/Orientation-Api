0dc50f418898f9a96904eafbf59306b92c875c71
Adam Smith <hagansmith@gmail.com>
2018-04-24 11:16:17 -0500

    computers table populated get all computers api complete

---
+++ BangazonOrientation/Controllers/ComputersController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BangazonOrientation.Models;
+using BangazonOrientation.Services;
+
+namespace BangazonOrientation.Controllers
+{
+    [RoutePrefix("api/computers")]
+    public class ComputersController : ApiController
+    {
+        [Route(""), HttpGet]
+        public HttpResponseMessage Get()
+        {
+            var repo = new ComputersRepository();
+            var results = repo.Get();
+
+            return results == null 
+                ? Request.CreateErrorResponse(HttpStatusCode.NoContent, "No computer records exist") 
+                : Request.CreateResponse(HttpStatusCode.OK, results);
+        }
+
+        //[Route("{id}"), HttpGet]
+        //public HttpResponseMessage Get(int id)
+        //{
+
+        //}
+
+        //[Route(""), HttpPost]
+        //public HttpResponseMessage Get(ComputersDto computer)
+        //{
+
+        //}
+
+        //[Route("{id}"), HttpPut]
+        //public HttpResponseMessage Put(ComputersDto computer)
+        //{
+
+        //}
+
+        //[Route("{id}"), HttpDelete]
+        //public HttpResponseMessage Delete(int id)
+        //{
+
+        //}
+    }
+}
---
+++ BangazonOrientation/Services/ComputersRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using BangazonOrientation.Models;
+using Dapper;
+
+namespace BangazonOrientation.Services
+{
+    public class ComputersRepository
+    {
+        private static SqlConnection GetConnection()
+        {
+            return new SqlConnection(ConfigurationManager.ConnectionStrings["BangazonOrientation"].ConnectionString);
+        }
+
+        public List<ComputersDto> Get()
+        {
+            using (var db = GetConnection())
+            {
+                db.Open();
+                var results = db.Query<ComputersDto>(@"select * from computers");
+
+                return results.ToList();
+            }
+        }
+    }
+}
