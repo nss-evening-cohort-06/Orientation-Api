52156c3da552c65a976dc495f479486b48c49485
Brooke Lewis <belv2c@gmail.com>
2018-04-25 19:33:22 -0500

    added department controller

---
+++ OrientationAPI/App/Controllers/DepartmentController.js
@@ -0,0 +1,11 @@
+﻿app.controller("DepartmentController", ["$scope", "$http",
+    function ($scope, $http) {
+        $scope.header = "Departments";
+
+        $http.get("/api/departments").then(function (result) {
+            $scope.departments = result.data;
+        }).catch(function (err) {
+            console.log(err);
+        });
+    }
+]);
