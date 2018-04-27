61fa7219f2d89f5145492a77ef5efdbef33844f8
Alex Sanchez <alesanchez728@gmail.com>
2018-04-27 01:10:32 -0500

    delete single computer works, roadblock: need a employeeComputer repo to check if it should be deleted or not

--- BangazonOrientation/app/Controllers/ComputersDetailController.js
+++ BangazonOrientation/app/Controllers/ComputersDetailController.js
@@ -1,14 +1,33 @@
-﻿app.controller("ComputersDetailController", ["$routeParams", "$scope", "$http",
-    function ($routeParams, $scope, $http) {
-        console.log($routeParams.id);
+﻿app.controller("ComputersDetailController", ["$location", "$routeParams", "$scope", "$http",
+    function ($location, $routeParams, $scope, $http) {
+
+        $scope.toDelete = false;
+        $scope.refuse = false;
+
         $http.get(`/api/computers/${$routeParams.id}`).then(function (result) {
             $scope.computer = result.data;
         });
 
-        /*  Given user is viewing a single computer
-            When the user clicks on the Delete link
-            Then the user should be presented with a screen to verify that it should be deleted
-          And if the user chooses Yes from that screen, the computer should be deleted only if it is has never been assigned to an employee
-    */
+        $scope.back = () => {
+            $location.path(`/computers`);
+        };
+
+        $scope.firstdelete = () => {
+
+            // check the join table,
+            //$http.get(`/api/EmployeeComputer/${$routeParams.id}`).then(function (result) {
+
+            //    // if a truthy result comes back that means that id exists so it cannot be deleted, as per project specs
+            //    // if it comes back falsy the id was not found, so the user can see the final delete button
+            //    result ? $scope.refuse = true : $scope.toDelete = true;
+            //});
+            $scope.toDelete = true;
+        };
+
+        $scope.theyReallyWantToDeleteThis = () => {
+            $http.delete(`/api/computers/${$routeParams.id}`).then(function (result) {
+                $location.path(`/computers`);
+            });
+        };
     }
 ]);
