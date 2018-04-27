e4c90785b08d9b6e386291826702613ea9ba53ae
Alex Sanchez <alesanchez728@gmail.com>
2018-04-27 11:37:14 -0500

    tried reverting original Db

--- Data/NewOrientationDb.sql
+++ Data/NewOrientationDb.sql
@@ -176,7 +176,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[EmployeeComputer](
-	[EmployeeComputerID] [int] IDENTITY(1,1) NOT NULL,
+	[EmployeeComputerID] [int] NOT NULL,
 	[AssignedDate] [datetime] NULL,
 	[ReturnedDate] [datetime] NULL,
 	[EmployeeID] [int] NULL,
