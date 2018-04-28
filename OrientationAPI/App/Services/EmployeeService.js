fd81240ddbe053bb37a9225510185112bf50d940
waamber <amberastuart@aol.com>
2018-04-27 20:59:12 -0500

    employee service js

--- OrientationAPI/App/Services/EmployeeService.js
+++ OrientationAPI/App/Services/EmployeeService.js
@@ -1,4 +1,14 @@
 ﻿app.service("EmployeeService", function ($http, $q, $rootScope) {
 
+    const addTEmployee = function (employee) {
+        return $q((resolve, reject) => {
+            $http.post(`http://localhost:50482/api/employees`, JSON.stringify(employee)).then(function (results) {
+                resolve(results);
+            }).catch(function (err) {
+                reject("error in addEmployee in Service", err);
+            });
+        });
+    }
 
+    return { addEmployee }
 });
