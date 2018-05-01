6132571cea38d6bd2dde6b1ff8ef42111506842d
Gina <ginamantonini@gmail.com>
2018-04-30 19:53:40 -0500

    create new department button is working

--- BangazonOrientation/app/Controllers/NewDepartmentsController.js
+++ BangazonOrientation/app/Controllers/NewDepartmentsController.js
@@ -1,1 +1,13 @@
-﻿
+﻿app.controller("NewDepartmentsController", ["$location", "$scope", "$http",
+    function ($location, $scope, $http) {
+
+        $scope.new = (Department) => {
+
+            $http.post("/api/departments/", Department).then(function () {
+                $location.path(`/departments`);
+            }).catch((err) => {
+                console.log("error posting new department", err);
+            });
+        };
+    }
+])
