115d6c06cc96cef41bcf25e6e80b3779b0fb3cb1
developer <matt.hensley777@gmail.com>
2018-04-30 20:19:40 -0500

    detail and add new

---
+++ BangazonOrientation/app/Controllers/NewEmployeeController.js
@@ -0,0 +1,12 @@
+﻿app.controller("NewEmployeeController", ["$location", "$scope", "$http",
+    function ($location, $scope, $http) {
+
+        $scope.new = (Employee) => {
+
+            $http.post("/api/employees/", Employee).then(function () {
+                $location.path(`/employees`);
+            }).catch((err) => {
+                console.log("error posting new employee", err);
+            });
+        };
+    }]);
