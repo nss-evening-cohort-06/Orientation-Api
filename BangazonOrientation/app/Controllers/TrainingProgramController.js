fb25bf032ddce248a8a56939bbb1de61dd417b36
lewiskg <kenneth.g.lewis2@gmail.com>
2018-05-04 16:28:27 -0500

    Finished with training program tickets.  Could use some validation for forms.

--- BangazonOrientation/app/Controllers/TrainingProgramController.js
+++ BangazonOrientation/app/Controllers/TrainingProgramController.js
@@ -31,8 +31,8 @@
 
             for (var i = 0, len = $scope.results.length; i < len; i++) {
                 startdate = $scope.results[i].StartDate;
-                $scope.results[i].StartDate = moment(startdate).format('MMMM Do YYYY, h:mm:ss a');
-                $scope.results[i].EndDate = moment($scope.results[i].EndDate).format('MMMM Do YYYY, h:mm:ss a');
+                $scope.results[i].StartDate = moment(startdate).format('MMMM D YYYY, h:mm:ss a');
+                $scope.results[i].EndDate = moment($scope.results[i].EndDate).format('MMMM D YYYY, h:mm:ss a');
                 $scope.results[i].isDisabled = moment(startdate).isAfter(new Date()) ? false : true;
                 grabEmployees(i, $scope.results[i].TrainingProgramID);
             }
@@ -47,26 +47,78 @@
             });
 
             $window.location.reload();
-        }
+        };
 
         $scope.CreateTraining = (newTraining) => {
-            //$http.post(`/api/trainingprogram/create`, newTraining).then(function () {
-            //    console.log(result);
-            //});
+            $scope.created = true;
 
-            //$window.location.reload();
+            newTraining.StartDate = convertDateTime(newTraining.StartDate);
+            newTraining.EndDate = convertDateTime(newTraining.EndDate);
 
             console.log(newTraining);
 
-        }
+            $http.post(`/api/trainingprogram/create`, JSON.stringify(newTraining)).then(function (result) {
+                console.log(result);
+            });
 
-        $scope.EditTraining = () => {
+            $window.location.reload();
 
-        }
+        };
 
+        const convertDateTime = (dt) => {
+            var x = moment(dt, 'MMMM D YYYY, h:mm:ss a').format();
+            var y = x.split("-");
+            y.pop();
+            return y.join("-");
+        };
 
+        $scope.EditTraining = (editedCourse) => {
+            $scope.edited = true;
+            var id = editedCourse.TrainingProgramID;
 
+            delete editedCourse.$$hashKey;
+            delete editedCourse.Employees;
+            delete editedCourse.isDisabled;
 
+            editedCourse.StartDate = convertDateTime(editedCourse.StartDate);
+            editedCourse.EndDate = convertDateTime(editedCourse.EndDate);
 
+            console.log(editedCourse);
+
+            $http.put(`/api/trainingprogram/${id}/edit`, JSON.stringify(editedCourse)).then(function (result) {
+                console.log(result);
+            });
+
+            editedCourse.StartDate = moment(editedCourse.StartDate).format('MMMM D YYYY, h:mm:ss a');
+            editedCourse.EndDate = moment(editedCourse.EndDate).format('MMMM D YYYY, h:mm:ss a');
+        };
+
+        $scope.undisable = () => {
+            $scope.edited = false;
+            $scope.created = false;
+
+        };
+
+        $scope.undisable = () => {
+            $scope.edited = false;
+        };
+
+        $scope.showThis = true;
+        $scope.showThat = true;
+
+        $scope.showAll = () => {
+            $scope.showThis = true;
+            $scope.showThat = true;
+        };
+
+        $scope.showPast = () => {
+            $scope.showThis = false;
+            $scope.showThat = true;
+        };
+
+        $scope.showFuture = () => {
+            $scope.showThis = true;
+            $scope.showThat = false;
+        };
     }
 ]);
